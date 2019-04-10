72638af5096b2968961edda9b1d37aaad982530b
RAHUL AGGARWAL <33187228+rahulagg26@users.noreply.github.com>
2019-04-10 23:04:18 +0530

    first commit

---
+++ welcome.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+
+Welcome <?php echo $_POST["firstname"]; ?>
+<?php echo $_POST["lastname"]; ?><br>
+You are from :<?php echo $_POST["country"]; ?><br>
+You're concern:<?php echo $_POST["subject"]; ?>
+</body>
+</html>
